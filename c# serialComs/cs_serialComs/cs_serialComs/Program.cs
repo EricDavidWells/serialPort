dbba5c33c6b31db03acf585c172737ae0970f071
Eric <ewells@ualberta.ca>
2019-06-25 19:26:06 -0600

    Header Check Added
    Added header checking, and timesync function.

--- c# serialComs/cs_serialComs/cs_serialComs/Program.cs
+++ c# serialComs/cs_serialComs/cs_serialComs/Program.cs
@@ -3,20 +3,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO.Ports;
+using System.Threading;
+
+// Based on the following: https://docs.microsoft.com/en-us/dotnet/api/system.io.ports.serialport.datareceived?view=netframework-4.8
+// 
 
 namespace cs_serialComs
 {
     class Program
     {
+        static System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+        static long prevtime = 0;
+        static long curtime = 0;
+
         static void Main(string[] args)
         {
             SerialPort mySerialPort = new SerialPort("COM6");
 
-            mySerialPort.BaudRate = 9600;
+            mySerialPort.BaudRate = 115200;
             mySerialPort.Parity = Parity.None;
             mySerialPort.StopBits = StopBits.One;
             mySerialPort.DataBits = 8;
             mySerialPort.Handshake = Handshake.None;
+            mySerialPort.ReadTimeout = -1;
 
             mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
 
@@ -29,10 +38,45 @@ namespace cs_serialComs
 
         private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
+            byte headercheck1 = 0x9F;
+            byte headercheck2 = 0x6E;
+            int msgsize = 2;
+
             SerialPort sp = (SerialPort)sender;
-            string indata = sp.ReadExisting();
-            Console.WriteLine("Data Received:");
-            Console.WriteLine(indata);
+            //int indata = sp.ReadByte();
+            //Console.WriteLine("Data Received:");
+            //Console.WriteLine(indata);
+
+            byte[] check1 = new byte[1];
+            byte[] check2 = new byte[1];
+
+            if (sp.BytesToRead > (msgsize + 2))
+            {
+                sp.Read(check1, 0, 1);
+                if (check1[0] == headercheck1)
+                {
+                    sp.Read(check2, 0, 1);
+                    if (check2[0] == headercheck2)
+                    {
+                        byte[] data = new byte[msgsize];
+                        int temp = sp.Read(data, 0, data.Length);
+
+                        if (temp == data.Length)
+                        {
+                            prevtime = curtime;
+                            curtime = sw.ElapsedMilliseconds;
+
+                            Console.Write("Data Received: ");
+                            Console.Write(System.Text.Encoding.Default.GetString(data, 0, data.Length));
+                            Console.Write(" Elapsed Time (ms): ");
+                            Console.Write(curtime-prevtime);
+                            //Console.Write(" Frequency: ");
+                            //Console.Write(1000 / (curtime - prevtime));
+                            Console.WriteLine();
+                        }  
+                    }
+                }
+            }
         }
     }
 }
--- serialComs/serialComs.ino
+++ serialComs/serialComs.ino
@@ -1,18 +1,24 @@
 int analogValue;
-int delayTime = 500;
+byte headercheck1 = 0x9F; // 159
+byte headercheck2 = 0x6E; // 110
+unsigned long timer = 0;
+long loopTimeMicroSec = 10000;
+
 
 void setup() {
   // Setup serial port
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 
 void loop() {
   // Read the analog pin
   analogValue = analogRead(A0);
+  analogValue = 300;
 
   // Write bytes via serial
   writeBytes(&analogValue);
+  timeSync(loopTimeMicroSec);
 }
 
 
@@ -21,10 +27,23 @@ void writeBytes(int* data1){
   byte* byteData1 = (byte*)(data1);
 
   // Byte array with header for transmission
-  byte buf[4] = {0x9F, 0x6E, byteData1[0], byteData1[1]};
+  byte buf[4] = {headercheck1, headercheck2, byteData1[0], byteData1[1]};
 
   // Write the byte
   Serial.write(buf, 4);
+}
 
-  delay(delayTime);
+void timeSync(unsigned long deltaT){
+  // Calculate required delay to run at 200 Hz
+  unsigned long currTime = micros();
+  long timeToDelay = deltaT - (currTime - timer);
+
+  if (timeToDelay > 5000){
+    delay(timeToDelay / 1000);
+    delayMicroseconds(timeToDelay % 1000);
+  } else if (timeToDelay > 0){
+    delayMicroseconds(timeToDelay);
+  } else {}
+
+  timer = currTime + timeToDelay;
 }
